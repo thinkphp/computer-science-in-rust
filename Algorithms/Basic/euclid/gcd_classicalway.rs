3eb95dc7c4c96381fc56a9fea32396ba640a2c79
Adrian Statescu <mergesortv@gmail.com>
2025-02-03 05:35:29 +0200

    Create gcd_classicalway.rs

---
+++ Algorithms/Basic/euclid/gcd_classicalway.rs
@@ -0,0 +1,13 @@
+fn gcd(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn main() {
+    let a = 56;
+    let b = 98;
+    println!("GCD of {} and {} is {}", a, b, gcd(a, b));
+}
