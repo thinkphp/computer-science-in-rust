84602c63172d717822db14fdc64b84caae68b65a
Adrian Statescu <mergesortv@gmail.com>
2025-02-04 08:11:26 +0200

    Create Palindrome-Checker.rs

---
+++ Algorithms/Basic/Palindrome-Checker/Palindrome-Checker.rs
@@ -0,0 +1,16 @@
+fn is_palindrome(s: &str) -> bool {
+    let s: Vec<char> = s
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(|c| c.to_ascii_lowercase())
+        .collect();
+    let (mut left, mut right) = (0, s.len().saturating_sub(1));
+    while left < right {
+        if s[left] != s[right] {
+            return false;
+        }
+        left += 1;
+        right -= 1;
+    }
+    true
+}
